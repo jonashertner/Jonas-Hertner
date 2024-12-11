a8e7ec22e5f08a56a5364c7481f6b0a9098e1912
jonashertner <130315719+jonashertner@users.noreply.github.com>
2024-12-12 00:06:24 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // script.js
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const password = prompt("Please enter the password:");
-//     if (password !== "tryanderror137") {
-//         document.body.innerHTML = "<h1>Access Denied</h1>";
-//         return;
-//     }
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    const password = prompt("Please enter the password:");
+    if (password !== "ff") {
+        document.body.innerHTML = "<h1>Access Denied</h1>";
+        return;
+    }
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     // Select essential DOM elements
